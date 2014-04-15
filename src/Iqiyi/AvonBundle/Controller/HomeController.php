e734c4e7413e5d5137a0b0b543dde7df27e4a18a
julius xue <yingang@thenetcircle.com>
2014-04-15 17:47:58 +0800

    n

---
+++ src/Iqiyi/AvonBundle/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Iqiyi\AvonBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('IqiyiAvonBundle:Home:index.html.twig');
+    }
+
+    public function addmsgAction()
+    {
+
+    }
+
+    public function addphotoAction()
+    {
+
+    }
+}
