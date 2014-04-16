2ff72f8af0fa36a632d8be59f0490b50a9606cb1
julius xue <yingang@thenetcircle.com>
2014-04-16 17:36:03 +0800

    add upload

--- src/Iqiyi/AvonBundle/Controller/HomeController.php
+++ src/Iqiyi/AvonBundle/Controller/HomeController.php
@@ -4,6 +4,10 @@ namespace Iqiyi\AvonBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Iqiyi\AvonBundle\Entity\AvonPhoto;
+
 class HomeController extends Controller
 {
     public function indexAction()
@@ -16,8 +20,34 @@ class HomeController extends Controller
 
     }
 
-    public function addphotoAction()
+    /**
+     * @Template()
+     */
+    public function addphotoAction(Request $request)
     {
+        $avonPhoto = new AvonPhoto();
+        $form = $this->createFormBuilder($avonPhoto)
+            ->setAction($this->generateUrl('iqiyi_avon_addphoto'))
+            ->add('memName', 'text', array('label'=>'姓名：', 'max_length'=>45))
+            ->add('memGender', 'choice', array('choices'   => array('0' => '男', '1' => '女'),
+                                                'required'  => true, 
+                                                'label'=>'性别：'))
+            ->add('memMobile', 'text', array( 'label'=>'手机：', 'max_length'=>15))
+            ->add('file', 'file', array( 'label'=>'照片：'))
+            ->add('save', 'submit', array( 'label'=>'发布'))
+            ->getForm();
 
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $avonPhoto->setAddTime(time());
+            $em->persist($avonPhoto);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('iqiyi_avon_homepage'));
+        }
+
+        return array('form' => $form->createView());
     }
 }
--- src/Iqiyi/AvonBundle/Entity/AvonPhoto.php
+++ src/Iqiyi/AvonBundle/Entity/AvonPhoto.php
@@ -4,6 +4,8 @@ namespace Iqiyi\AvonBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 /**
  * AvonPhoto
  */
@@ -163,5 +165,115 @@ class AvonPhoto
     public function getAddTime()
     {
         return $this->addTime;
+    }
+
+//==================================================================
+
+    private $file;
+    private $temp;
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload()
+    {
+        if (null !== $this->getFile()) {
+            // do whatever you want to generate a unique name
+            $filename = sha1(uniqid(mt_rand(), true));
+            $this->photoUrl = $filename.'.'.$this->getFile()->guessExtension();
+        }
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
+    public function upload()
+    {
+        // the file property can be empty if the field is not required
+        if (null === $this->getFile()) {
+            return;
+        }
+
+        // if there is an error when moving the file, an exception will
+        // be automatically thrown by move(). This will properly prevent
+        // the entity from being persisted to the database on error
+        $this->getFile()->move($this->getUploadRootDir(), $this->photoUrl);
+
+        // check if we have an old image
+        if (isset($this->temp)) {
+            // delete the old image
+            unlink($this->getUploadRootDir().'/'.$this->temp);
+            // clear the temp image path
+            $this->temp = null;
+        }
+        $this->file = null;
+    }
+
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeUpload()
+    {
+        if ($file = $this->getAbsolutePath()) {
+            unlink($file);
+        }
+    }
+
+    /**
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+        // check if we have an old image path
+        if (isset($this->photoUrl)) {
+            // store the old name to delete after the update
+            $this->temp = $this->path;
+            $this->path = null;
+        } else {
+            $this->path = 'initial';
+        }
+    }
+
+    /**
+     * Get file.
+     *
+     * @return UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    public function getAbsolutePath()
+    {
+        return null === $this->photoUrl
+            ? null
+            : $this->getUploadRootDir().'/'.$this->photoUrl;
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->photoUrl
+            ? null
+            : $this->getUploadDir().'/'.$this->photoUrl;
+    }
+
+    protected function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/documents';
     }
 }
