009c6c433b4b879b67f6982fd357364300ab79b8
julius xue <yingang@thenetcircle.com>
2014-04-17 17:04:09 +0800

    add mobile unique

--- src/Iqiyi/AvonBundle/Controller/HomeController.php
+++ src/Iqiyi/AvonBundle/Controller/HomeController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Iqiyi\AvonBundle\Entity\AvonPhoto;
+use Iqiyi\AvonBundle\Entity\AvonSubject;
 
 class HomeController extends Controller
 {
@@ -15,9 +16,38 @@ class HomeController extends Controller
         return $this->render('IqiyiAvonBundle:Home:index.html.twig');
     }
 
-    public function addmsgAction()
+    /**
+    *  @Template()
+    */
+    public function addmsgAction(Request $request)
     {
-        //留下最美时刻，填写手机号，姓氏，性别，地址，邮编信息
+        $avonSubject = new AvonSubject();
+        $form = $this->createFormBuilder($avonSubject)
+            ->setAction($this->generateUrl('iqiyi_avon_addmsg'))
+            ->add('memName', 'text', array('label'=>'姓名：', 'max_length'=>45))
+            ->add('memGender', 'choice', array('choices'   => array('0' => '男', '1' => '女'),
+                                                'required'  => true, 
+                                                'label'=>'性别：'))
+            ->add('memMobile', 'text', array( 'label'=>'手机：', 'max_length'=>15))
+            ->add('memAddress', 'text', array( 'label'=>'地址：', 'max_length'=>60))
+            ->add('memZip', 'text', array( 'label'=>'邮编：', 'max_length'=>15))
+            ->add('content', 'textarea', array( 'label'=>'我的瞬间：'))
+            ->add('save', 'submit', array( 'label'=>'发布'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $avonSubject->setAddTime(time());
+            $avonSubject->setFromType(0);
+            $em->persist($avonSubject);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('iqiyi_avon_homepage'));
+        }
+
+        return array('form' => $form->createView());
     }
 
     public function votemsgAction()
