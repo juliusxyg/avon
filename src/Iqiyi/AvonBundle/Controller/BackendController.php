89201e1448970c87b32a040b71918f9e3b1f5d8f
julius xue <yingang@thenetcircle.com>
2014-04-24 11:35:15 +0800

    add http authentication

--- src/Iqiyi/AvonBundle/Controller/BackendController.php
+++ src/Iqiyi/AvonBundle/Controller/BackendController.php
@@ -5,6 +5,7 @@ namespace Iqiyi\AvonBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Iqiyi\AvonBundle\Entity\AvonRedeemCode;
@@ -28,6 +29,32 @@ class BackendController extends Controller
 	    $em->clear();
 	  }
     return new Response("生成了2000个码");
+  }
+
+  /**
+  *  @Template()
+  */
+  public function loginAction(Request $request)
+  {
+  	$defaultData = array();
+    $form = $this->createFormBuilder($defaultData)
+        ->add('username', 'text')
+        ->add('password', 'password')
+        ->add('save', 'submit', array( 'label'=>'登入'))
+        ->getForm();
+
+    if ($request->isMethod('POST')) {
+        $form->bind($request);
+
+        $data = $form->getData();
+        if($data['username']=='admin' && $data['password']==date("YmdHi")){
+
+        }else{
+        	$form->addError(new FormError('用户名或密码错误'));
+        }
+    }
+
+    return array('form'=>$form->createView());
   }
 }
 ?>
